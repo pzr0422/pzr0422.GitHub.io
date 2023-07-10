0edc651037861e70d2c376d584fafa196be8942f
pzr0422 <138178395+pzr0422@users.noreply.github.com>
2023-07-10 15:25:42 +0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,43 @@
+
+
+let myImage = document.querySelector('img');
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/image1.jpg'){
+        myImage.setAttribute('src', 'images/image2.jpg');
+    }else {
+        myImage.setAttribute('src', 'images/image1.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName()
+{
+    let myName = prompt('请输入你的名字：');
+    if(!myName)
+    {
+        setUserName();
+    }
+    else
+    {
+        localStorage.setItem('name',myName);
+        myHeading.textContent = 'pzr酷毙了，' +myName;
+    }
+}
+
+if(!localStorage.getItem('name'))
+{
+    setUserName();
+}
+else
+{
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'pzr酷毙了，' + storedName;
+}
+
+
+myButton.onclick = function() {
+    setUserName();
+}
